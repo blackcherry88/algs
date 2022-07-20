1ea1acfcd9e5952a2483df3bf8c17d896e5f9516
Tao Hu <taohu88@gmail.com>
2022-07-20 13:46:17 -0700

    Add reverse words

---
+++ src/ReverseWords.java
@@ -0,0 +1,32 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ReverseWords {
+    static final Logger logger = LogManager.getLogger(ReverseWords.class.getName());
+
+    public static void reverseChar(char[] s, int i, int j) {
+        while (i < j) {
+            char t = s[i];
+            s[i]= s[j];
+            s[j] = t;
+            i += 1;
+            j -= 1;
+        }
+    }
+
+    public static void reverseWords(char[] s) {
+        reverseChar(s, 0, s.length-1);
+        int i = 0;
+        for (int j = 0; j < s.length; j++) {
+            if (s[j] == ' ') {
+                reverseChar(s, i, j-1);
+                i = j+1;
+            }
+        }
+        reverseChar(s, i, s.length-1);
+    }
+
+    public static void main(String[] argv) {
+        logger.info("Test logging {}", "great");
+    }
+}
